eb0360677a2d2ad592da3fce01c1623291aad20f
Lilian501 <llafin@gaming.tech>
2023-11-16 21:09:31 +0100

    nettoyer opti

--- sfml/main.cpp
+++ sfml/main.cpp
@@ -1,21 +1,11 @@
-#include "gameObject.h"
-#include "math.h"
-#include "windowManager.h"
 #include "gameManager.h"
-
-#include <SFML/Graphics.hpp>
 #include <iostream>
-
 
 int main(int argc, char** argv)
 {
-    //
-
     //init
     GameManager::Init();
-
-    GameManager::Get()->mainLoop();
-    
+    GameManager::Get()->mainLoop();  
     return 0;
 }
 
